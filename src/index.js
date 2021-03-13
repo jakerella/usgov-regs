2b518d8549562976511101048b5cdeddaf628f2c
Jordan Kasper <jordan@jordankasper.com>
2021-03-13 14:40:28 -0500

    Fully implemented USWDS for home page and layout; and fixed it up for docket page

--- src/index.js
+++ src/index.js
@@ -35,7 +35,9 @@ app.use((req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-    console.error(err)
+    if (!err.status || err.status > 499) {
+        console.error(err)
+    }
     
     res.status(err.status || 500)
     res.render('error', {
